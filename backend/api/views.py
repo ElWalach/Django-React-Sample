b9fa76dd80d2edf4b8d6e64c5f9177d581a90f1a
ElWalach <leonwalach653@gmail.com>
2026-03-22 13:26:45 +0100

    stworzenie registartion view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset= User.objects.all()     #zeby nie bylo takiego samego usera
+    serializer_class= User  #info co trzeba zeby zrobic takiego usera
+    permission_Classes= [AllowAny]  #kazdy ma mozliwosc zrobic nowego usera
